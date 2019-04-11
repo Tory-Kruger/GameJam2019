5b99b5722cf13c99ce523a86676ef5841e365839
Cris-Bwls <36465203+Cris-Bwls@users.noreply.github.com>
2019-04-11 14:32:09 +1000

    Modified Respawn.cs - respawns player properly now

--- GameJamGame/Assets/Scripts/Respawn.cs
+++ GameJamGame/Assets/Scripts/Respawn.cs
@@ -8,20 +8,30 @@ public class Respawn : MonoBehaviour {
 	public float _respawnHeight = -1000.0f;
 	public Transform _resetPos;
 
-	private float timer;
-	
+	public float timer = 0;
+
 	// Update is called once per frame
 	void Update ()
 	{
 		bool reset = false;
 
-		if (Physics.Raycast(transform.position, Vector3.down, 100.0f))
+		if (!(Physics.Raycast(transform.position, Vector3.down, 10000.0f)) && timer < float.Epsilon)
 			timer = Time.time;
 
-		reset |= ((timer - Time.time) > _respawnTime);
-		reset |= transform.position.y < _respawnHeight;
+		if ((timer - Time.time) > _respawnTime && timer > 0)
+			reset = true;
+
+		if (transform.position.y < _respawnHeight)
+			reset = true;
 
 		if (reset)
-			transform.position = _resetPos.position;
+			Reset();
+	}
+
+	private void Reset()
+	{
+		transform.position = _resetPos.position;
+		GetComponent<CharacterController>().transform.position = _resetPos.position;
+		timer = 0;
 	}
 }

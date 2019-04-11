3a4d6d637c915179efa7474e174fb395ebbbcee1
Cris-Bwls <36465203+Cris-Bwls@users.noreply.github.com>
2019-04-11 13:50:02 +1000

    Added Respawn.cs - Respawns Player if they fall below a certain height or if they are over the void for too long

---
+++ GameJamGame/Assets/Scripts/Respawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+	public float _respawnTime = 2.0f;
+	public float _respawnHeight = -1000.0f;
+	public Transform _resetPos;
+
+	private float timer;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		bool reset = false;
+
+		if (Physics.Raycast(transform.position, Vector3.down, 100.0f))
+			timer = Time.time;
+
+		reset |= ((timer - Time.time) > _respawnTime);
+		reset |= transform.position.y < _respawnHeight;
+
+		if (reset)
+			transform.position = _resetPos.position;
+	}
+}

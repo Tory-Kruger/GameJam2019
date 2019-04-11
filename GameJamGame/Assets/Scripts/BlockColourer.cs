01f44727f8e13e4dc6cae9710c263cdb2016cdb1
Cris-Bwls <36465203+Cris-Bwls@users.noreply.github.com>
2019-04-11 10:59:30 +1000

    Added BlockColourer.cs

---
+++ GameJamGame/Assets/Scripts/BlockColourer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockColourer : MonoBehaviour {
+
+    private string currentTag = "Uncolored";
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+		if (collision.collider.tag == "Colored")
+		{
+			string tag = collision.collider.tag;
+
+			if (currentTag == tag) return;
+
+			// Notify GameManager of color Change
+
+			// Set new Color according to tag
+		}
+    }
+}

8ea0cda64ea27f2500f703e39aeca8c59fa6c3f7
Cris-Bwls <36465203+Cris-Bwls@users.noreply.github.com>
2019-04-11 14:53:19 +1000

    removed time aspect from Respawn

--- GameJamGame/Assets/Scripts/Respawn.cs
+++ GameJamGame/Assets/Scripts/Respawn.cs
@@ -4,33 +4,13 @@ using UnityEngine;
 
 public class Respawn : MonoBehaviour {
 
-	public float _respawnTime = 2.0f;
-	public float _respawnHeight = -1000.0f;
+	public float _respawnHeight = -20.0f;
 	public Transform _resetPos;
-
-	public float timer = 0;
 
 	// Update is called once per frame
 	void Update ()
 	{
-		bool reset = false;
-
-		if (!(Physics.Raycast(transform.position, Vector3.down, 10000.0f)) && timer < float.Epsilon)
-			timer = Time.time;
-
-		if ((Time.time - timer) > _respawnTime && timer > 0)
-			reset = true;
-
 		if (transform.position.y < _respawnHeight)
-			reset = true;
-
-		if (reset)
-			Reset();
-	}
-
-	private void Reset()
-	{
-		transform.position = _resetPos.position;
-		timer = 0;
+			transform.position = _resetPos.position;
 	}
 }

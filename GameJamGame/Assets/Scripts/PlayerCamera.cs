003e05539b58b89f48c97611716e532fcc60e369
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2019-04-11 15:15:29 +1000

    PlayerCamera script added

---
+++ GameJamGame/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+	public Transform target;
+	public float smoothTime = 0.04f;
+	public float slerpT = 0.1f;
+
+	private Vector3 velocity;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.SmoothDamp(transform.position, target.position, ref velocity, smoothTime);
+		transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, slerpT);
+	}
+}
